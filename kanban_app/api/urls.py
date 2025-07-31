35532ebc16c49a09506caf9f3a83a791490e225b
Nicolas Voges <nicolas.voges@hotmail.com>
2025-07-31 13:56:22 +0200

    Feat: Prepare URLs in kanban_app.

---
+++ kanban_app/api/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+#from .views import 
+
+# urlpatterns = [
+#     path('boards/', .as_view(), name='boards-list'),
+#     path('boards/<int:pk>/', .as_view(), name='board-detail'),
+#     path('tasks/<int:pk>/', .as_view(), name='task-detail'),
+#     path('tasts/assigned-to-me/', .as_view(), name='assigned-to-me')
+#     path('tasts/reviewing/', .as_view(), name='review')
+#     path('email-check/', .as_view(), name='email-check')
+# ]
