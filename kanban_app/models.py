b04ffb79852b13c69bf0ec3e6483e70553966406
Nicolas Voges <nicolas.voges@hotmail.com>
2025-07-31 18:06:26 +0200

    Refactor: Adjust models in kanban_app.

--- kanban_app/models.py
+++ kanban_app/models.py
@@ -4,8 +4,14 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 from django.core.exceptions import ValidationError
 
 class Board(models.Model):
-    members = models.ManyToManyField(UserAccount, on_delete=models.CASCADE)
-    
+    title = models.CharField(max_length=63)
+    members_list = models.ManyToManyField(UserAccount, on_delete=models.CASCADE)
+    member_count = models.IntegerField()
+    ticket_count = models.IntegerField()
+    tasks_to_do_count = models.IntegerField()
+    tasks_high_prio_count = models.IntegerField()
+    owner_id = models.ForeignKey(UserAccount, on_delete=models.CASCADE)
+
 
 class Task(models.Model):
     board = models.ForeignKey(Board, on_delete=models.CASCADE)
@@ -13,8 +19,21 @@ class Task(models.Model):
     description = models.CharField(max_length=127)
     status = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(3)])
     priority = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(2)])
-    assignee = models.ManyToManyField(UserAccount, on_delete=models.CASCADE)
-    reviewer = models.ManyToManyField(UserAccount, on_delete=models.CASCADE)
+    assignee = models.ForeignKey(
+        UserAccount,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name='tasks_assigned'
+    )
+
+    reviewer = models.ForeignKey(
+        UserAccount,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name='tasks_reviewed'
+    )
     due_date = models.DateField()
 
 
@@ -31,7 +50,5 @@ class Comment(models.Model):
     )
 
     def clean(self):
-
-        # Genau ein Ziel muss gesetzt sein: entweder board oder task, nicht beides
         if (self.board and self.task) or (not self.board and not self.task):
             raise ValidationError('A comment must be assigned to either a board or a task – not both and not neither.')
