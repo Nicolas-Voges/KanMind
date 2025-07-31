71049a2923211a0f6e8d1f465d1d1fc68148559f
Nicolas Voges <nicolas.voges@hotmail.com>
2025-07-31 18:12:36 +0200

    Feat: Add router in urls.py and implement BoardViewSet.

--- kanban_app/api/urls.py
+++ kanban_app/api/urls.py
@@ -1,11 +1,21 @@
-from django.urls import path
-#from .views import 
+from django.urls import path, include
+from rest_framework import routers
+from .views import BoardViewSet
 
-# urlpatterns = [
-#     path('boards/', .as_view(), name='boards-list'),
-#     path('boards/<int:pk>/', .as_view(), name='board-detail'),
-#     path('tasks/<int:pk>/', .as_view(), name='task-detail'),
-#     path('tasts/assigned-to-me/', .as_view(), name='assigned-to-me')
-#     path('tasts/reviewing/', .as_view(), name='review')
-#     path('email-check/', .as_view(), name='email-check')
-# ]
+router = routers.SimpleRouter()
+router.register(r'Boards', BoardViewSet)
+
+urlpatterns = [
+    path('boards', include(router.urls))
+#     path('boards/', .as_view(), name='boards-list'),                                    POST GET
+#     path('boards/<int:pk>/', .as_view(), name='board-detail'),                          GET PATCH DELETE
+
+#     path('tasks/', .as_view(), name='task-list'),                                       POST
+#     path('tasks/<int:pk>/', .as_view(), name='task-detail'),                            PATCH DELETE
+#     path('tasts/<int:pk>/comments/', .as_view(), name='review')                         GET POST
+#     path('tasts/<int:task_id>/comments/<int:comment_id>/', .as_view(), name='review')   DELETE
+#     path('tasts/assigned-to-me/', .as_view(), name='assigned-to-me')                    GET
+#     path('tasts/reviewing/', .as_view(), name='review')                                 GET
+
+#     path('email-check/', .as_view(), name='email-check')                                GET
+]
