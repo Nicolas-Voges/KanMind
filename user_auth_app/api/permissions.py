7e6b75694782487b87c88b3c9ca2396de09a6a02
Nicolas Voges <nicolas.voges@hotmail.com>
2025-08-07 18:09:55 +0200

    Feat: Add task permissions.

--- user_auth_app/api/permissions.py
+++ user_auth_app/api/permissions.py
@@ -1,28 +1,20 @@
+from django.contrib.auth.models import User
 from rest_framework.permissions import BasePermission
-from kanban_app.models import Board
+from kanban_app.models import Board, Task
 
-class IsMemberOrOwner(BasePermission):
+class IsBoardMemberOrOwner(BasePermission):
     def has_object_permission(self, request, view, obj):
 
         if request.method == 'DELETE':
-            return bool(request.user and (request.user.id == obj.owner_id or request.user.is_superuser))
+            return bool(request.user and (request.user == obj.owner or request.user.is_superuser))
         else:
             is_owner = bool(request.user.id == obj.owner_id)
             is_member = obj.members.filter(id=request.user.id).exists()
             return is_owner or is_member
         
 
-class IsMember(BasePermission):
+class IsTaskBoardMember(BasePermission):
     def has_permission(self, request, view):
-        # return True
-        # if request.method == 'POST':
-        #     return bool(request.user.id and request.user.id in obj.board.members.all())
-        # else:
-        #     return False
-
-        if request.method != 'POST':
-            return False
-
         board_id = request.data.get('board')
         if not board_id:
             return False
@@ -33,3 +25,38 @@ class IsMember(BasePermission):
             return False
 
         return request.user in board.members.all()
+    
+
+class IsTaskBoardOwner(BasePermission):
+    def has_permission(self, request, view):
+
+        board_id = request.data.get('board')
+        if not board_id:
+            return False
+
+        try:
+            board = Board.objects.get(id=board_id)
+        except Board.DoesNotExist:
+            return False
+
+        return request.user == board.owner
+    
+
+class IsTaskCreator(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        creator_id = request.data.get('creator')
+        if not creator_id:
+            return False
+        try:
+            creator = User.objects.get(id=creator_id)
+        except User.DoesNotExist:
+            return False
+        
+        return request.user == creator
+    
+
+class IsTaskOwnerOrCreator(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        board = obj.board
+        creator = obj.creator
+        return request.user in [board.owner, creator]
