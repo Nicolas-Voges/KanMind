09f388b50a5f9bb03e1acf19a6de7b064368a4a5
Nicolas Voges <nicolas.voges@hotmail.com>
2025-08-07 18:51:34 +0200

    Feat: Add URLs.

--- kanban_app/api/urls.py
+++ kanban_app/api/urls.py
@@ -1,18 +1,19 @@
 from django.urls import path, include
 from rest_framework import routers
-from .views import BoardViewSet, TaskCreateView
+from .views import BoardViewSet, TaskCreateView, TaskDetailUpdateDestroyView, TaskGetDetailView, \
+CommentCreateListView, CommentDestroyView
 
 router = routers.SimpleRouter()
 router.register(r'boards', BoardViewSet, basename='board')
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('tasks/', TaskCreateView.as_view(), name='task-post'),                               # POST
-    # path('tasks/<int:pk>/', .as_view(), name='task-detail'),                          # PATCH DELETE
-#     path('tasts/<int:pk>/comments/', .as_view(), name='review')                       # GET POST
-#     path('tasts/<int:task_id>/comments/<int:comment_id>/', .as_view(), name='review') # DELETE
-#     path('tasts/assigned-to-me/', .as_view(), name='assigned-to-me')                  # GET
-#     path('tasts/reviewing/', .as_view(), name='review')                               # GET
+    path('tasks/', TaskCreateView.as_view(), name='task-post'),
+    path('tasks/<int:pk>/', TaskDetailUpdateDestroyView.as_view(), name='task-detail'),
+    path('tasks/assigned-to-me/', TaskGetDetailView.as_view(), name='assigned-to-me'),
+    path('tasks/reviewing/', TaskGetDetailView.as_view(), name='review'),
+    path('tasks/<int:task_id>/comments/', CommentCreateListView.as_view(), name='review'),
+#     path('tasks/<int:task_id>/comments/<int:comment_id>/', .as_view(), name='review')     # DELETE
 
-#     path('email-check/', .as_view(), name='email-check')                              # GET
+#     path('email-check/', .as_view(), name='email-check')                                  # GET
 ]
