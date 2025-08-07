73653825b96d14097109dcf716cc85415f818388
Nicolas Voges <nicolas.voges@hotmail.com>
2025-08-07 18:46:51 +0200

    Refactor: Adjust task view and imports.

--- kanban_app/api/views.py
+++ kanban_app/api/views.py
@@ -1,16 +1,21 @@
-from rest_framework import viewsets
+from django.utils import timezone
+from django.db.models import Q
+from rest_framework import viewsets, status, generics, mixins
+from rest_framework.response import Response
+from rest_framework.views import APIView
 from rest_framework.decorators import api_view
 from rest_framework.permissions import IsAuthenticated
-from rest_framework import generics
-from django.db.models import Q
-from kanban_app.models import Board, Task
+from rest_framework.generics import GenericAPIView
+from user_auth_app.api.permissions import IsBoardMemberOrOwner, IsTaskBoardMember, \
+    IsTaskOwnerOrCreator, IsCommentBoardMember
+from kanban_app.models import Board, Task, Comment
 from .serializers import BoardSerializer, BoardDetailSerializer, \
-TaskSerializer
-from user_auth_app.api.permissions import IsMemberOrOwner, IsMember
+    TaskSerializer, CommentSerializer
+
 
 class BoardViewSet(viewsets.ModelViewSet):
-    permission_classes = [IsAuthenticated, IsMemberOrOwner]
-    
+    permission_classes = [IsAuthenticated, IsBoardMemberOrOwner]
+
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
 
@@ -18,15 +23,19 @@ class BoardViewSet(viewsets.ModelViewSet):
         if self.action in ['retrieve', 'partial_update', 'update']:
             return BoardDetailSerializer
         return BoardSerializer
-    
+
     def get_queryset(self):
         user = self.request.user
         return Board.objects.filter(
             Q(owner=user) | Q(members=user)
         ).distinct()
-    
+
 
 class TaskCreateView(generics.CreateAPIView):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
-    permission_classes = [IsAuthenticated, IsMember]
+    permission_classes = [IsAuthenticated, IsTaskBoardMember]
+
+    def perform_create(self, serializer):
+        serializer.save(creator=self.request.user)
+
