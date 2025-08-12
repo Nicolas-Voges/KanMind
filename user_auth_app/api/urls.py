e882d2c1731e381f410b0250e2e2b7a13d1d37c4
Nicolas Voges <nicolas.voges@hotmail.com>
2025-08-12 13:38:26 +0200

    Feat: Add login.

--- user_auth_app/api/urls.py
+++ user_auth_app/api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
+from rest_framework.authtoken.views import obtain_auth_token
 from .views import RegistrationView
 
 urlpatterns = [
     path('registration/', RegistrationView.as_view(), name='registration'),
-    # path('login/', .as_view(), name='login')
+    path('login/', obtain_auth_token, name='login')
 ]
