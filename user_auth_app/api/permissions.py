1a6c3663f5107abe493644b2b1d91ffee88db3a3
Nicolas Voges <nicolas.voges@hotmail.com>
2025-08-14 01:13:53 +0200

    Docs: Add docs in auth app permissions.

--- user_auth_app/api/permissions.py
+++ user_auth_app/api/permissions.py
@@ -1,3 +1,11 @@
+"""
+Custom permission classes for the Kanban application.
+
+These permissions restrict access to boards, tasks, and comments
+based on user roles, board membership, and ownership.
+"""
+
+
 from django.contrib.auth.models import User
 from rest_framework.permissions import BasePermission
 from rest_framework.generics import get_object_or_404
@@ -5,8 +13,17 @@ from rest_framework.exceptions import NotFound
 from kanban_app.models import Board, Task
 
 class IsBoardMemberOrOwner(BasePermission):
-    def has_object_permission(self, request, view, obj):
+    """
+    Permission to check if the requesting user is a board member or owner.
+    """
 
+
+    def has_object_permission(self, request, view, obj):
+        """
+        Allow access if:
+        - The request is DELETE and the user is the owner or a superuser.
+        - For other methods, the user is either the board owner or a member.
+        """
         if request.method == 'DELETE':
             return bool(request.user and (request.user == obj.owner or request.user.is_superuser))
         else:
@@ -16,7 +33,16 @@ class IsBoardMemberOrOwner(BasePermission):
         
 
 class IsTaskBoardMember(BasePermission):
+    """
+    Permission to check if the requesting user is a member of the task's board.
+    """
+
+
     def has_object_permission(self, request, view, obj):
+        """
+        Allow access if the user is a member of the board associated with the task.
+        Raise a NotFound error if the board does not exist.
+        """
         board_id = obj.board_id
         if not board_id:
             raise NotFound("Board ID not provided.")
@@ -30,8 +56,15 @@ class IsTaskBoardMember(BasePermission):
     
 
 class IsTaskBoardOwner(BasePermission):
-    def has_permission(self, request, view):
+    """
+    Permission to check if the requesting user is the owner of the task's board.
+    """
 
+
+    def has_permission(self, request, view):
+        """
+        Allow access if the user is the owner of the board specified in the request data.
+        """
         board_id = request.data.get('board')
         if not board_id:
             return False
@@ -45,7 +78,15 @@ class IsTaskBoardOwner(BasePermission):
     
 
 class IsTaskCreator(BasePermission):
+    """
+    Permission to check if the requesting user is the creator of the task.
+    """
+
+
     def has_object_permission(self, request, view, obj):
+        """
+        Allow access if the user is the creator specified in the request data.
+        """
         creator_id = request.data.get('creator')
         if not creator_id:
             return False
@@ -58,14 +99,33 @@ class IsTaskCreator(BasePermission):
     
 
 class IsTaskOwnerOrCreator(BasePermission):
+    """
+    Permission to check if the requesting user is the owner of the board
+    or the creator of the task.
+    """
+
+
     def has_object_permission(self, request, view, obj):
+        """
+        Allow access if the user is the board owner or the task creator.
+        """
         board = obj.board
         creator = obj.creator
         return request.user in [board.owner, creator]
     
 
 class IsCommentBoardMember(BasePermission):
+    """
+    Permission to check if the requesting user is a member of the board
+    associated with the comment's task.
+    """
+
+
     def has_permission(self, request, view):
+        """
+        Allow access if the user is a member of the board linked to the given task.
+        Raise a 404 error if the task does not exist.
+        """
         user = request.user
         task_id = view.kwargs['task_id']
         task = get_object_or_404(Task, pk=task_id)
--- user_auth_app/models.py
+++ user_auth_app/models.py
@@ -1,12 +1,1 @@
 from django.db import models
-# from django.contrib.auth.models import User
-
-# class UserAccount(models.Model):
-#     user = models.OneToOneField(User, on_delete=models.CASCADE)
-#     fullname = models.CharField(max_length=63)
-#     email = models.CharField(max_length=63)
-#     password = models.CharField(max_length=63)
-#     repeated_password = models.CharField(max_length=63)
-    
-#     def __str__(self):
-#         return self.user.username
